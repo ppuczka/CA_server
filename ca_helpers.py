76fb6625473e34b282cf9b39939dad5cae89fa7e
Przemyslaw Puczka <p.puczka@gmail.com>
2020-03-16 22:04:45 +0100

    sign csr method

--- ca_helpers.py
+++ ca_helpers.py
@@ -1,46 +1,35 @@
 from datetime import datetime, timedelta
 
-from cryptography import x509
 from cryptography.hazmat.backends import default_backend
-
-from cryptography.x509.oid import NameOID
-
+from cryptography.hazmat.backends.openssl import x509
 from cryptography.hazmat.primitives import hashes, serialization
 
+# we have to load ca_public_key & ca_private_key form Azure vault, also new file should be able to download
+# or send to request owner
 
-def generate_public_key(private_key, filename, **kwargs):
-    subject = x509.Name(
 
-        [
-            x509.NameAttribute(NameOID.COUNTRY_NAME, kwargs["country"]),
-            x509.NameAttribute(
-                NameOID.STATE_OR_PROVINCE_NAME, kwargs["state"]
-            ),
-            x509.NameAttribute(NameOID.LOCALITY_NAME, kwargs["locality"]),
-            x509.NameAttribute(NameOID.ORGANIZATION_NAME, kwargs["org"]),
-            x509.NameAttribute(NameOID.COMMON_NAME, kwargs["hostname"]),
-        ]
-    )
-
-    issuer = subject
+def sign_csr(csr, ca_public_key, ca_private_key, new_filename):
     valid_from = datetime.utcnow()
-    valid_to = valid_from + timedelta(days=30)
+    valid_until = valid_from + timedelta(days=30)
 
     builder = (
         x509.CertificateBuilder()
-        .subject_name(subject)
-        .issuer_name(issuer)
-        .public_key(private_key.public_key())
+        .subject_name(csr.subject)
+        .issuer_name(ca_public_key.subject)
+        .public_key(csr.public_key())
         .serial_number(x509.random_serial_number())
         .not_valid_before(valid_from)
-        .not_valid_after(valid_to)
+        .not_valid_after(valid_until)
     )
+
+    for extension in csr.extensions:
+        builder = builder.add_extension(extension.value, extension.critical)
 
     public_key = builder.sign(
-       private_key, hashes.SHA256(), default_backend()
+        private_key=ca_private_key,
+        algorithm=hashes.SHA256(),
+        backend=default_backend(),
     )
 
-    with open(filename, "wb") as certfile:
-        certfile.write(public_key.public_bytes(serialization.Encoding.PEM))
-
-    return public_key
+    with open(new_filename, "wb") as keyfile:
+        keyfile.write(public_key.public_bytes(serialization.Encoding.PEM))
