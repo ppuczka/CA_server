2bac1e3f593f5df621856b0f8a4d5ee02496b4ef
Przemyslaw Puczka <p.puczka@gmail.com>
2020-03-18 22:44:51 +0100

    REST api workshop

--- ca_server_app.py
+++ ca_server_app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify, request
 
 ca_server_app = Flask(__name__)
 
@@ -6,3 +6,19 @@ ca_server_app = Flask(__name__)
 @ca_server_app.route('/')
 def main_page():
     return 'Welcome to the main page test'
+
+
+@ca_server_app.route('/api/ca/certificate', methods=['GET', 'POST'])
+def rest_page():
+    if request.method == 'GET':
+        return 'Welcome'
+    elif request.method == 'POST':
+        csr = request.args.get('csr', '')
+        print(f'This is where csr should be {csr}')
+        return csr
+#
+#
+# @ca_server_app.errorhandler('404')
+# def not_found():
+#     return make_response(jsonify({'error': 'Not found'}), 404)
+
