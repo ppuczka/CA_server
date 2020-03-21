38da9f15c5953233e4db7d814440edc16f103e6c
Przemyslaw Puczka <p.puczka@gmail.com>
2020-03-21 21:42:05 +0100

    REST api workshop working Rest endpoint

--- ca_app.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from flask_restful import Api
-
-from classes import CaServer
-
-api_blueprint = Blueprint('api', __name__)
-api = Api(api_blueprint)
-
-api.add_resource(CaServer, '/ca')
--- classes/CaServer.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import Resource
-
-
-class CaServer(Resource):
-
-    def get(self):
-        return {'message' : 'this is ca server message'}
-
